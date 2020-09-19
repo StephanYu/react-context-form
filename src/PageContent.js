b4e5aa7d7a0da45366405ca21c4dcdcbfb581e81
Stephan Yue <stephan.yu@gmail.com>
2020-09-19 09:14:48 +0200

    Add toggleTheme() to navbar switch for dynamic context changing

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 export default class PageContent extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isDarkTheme } = this.context;
+
     const styles = {
-      backgroundColor: "white",
+      backgroundColor: isDarkTheme ? "rgb(64, 72, 79)" : "white",
       height: "100vh",
       width: "100wh",
     };
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -8,10 +8,18 @@ export class ThemeProvider extends Component {
     this.state = {
       isDarkTheme: true,
     };
+    this.toggleTheme = this.toggleTheme.bind(this);
   }
+
+  toggleTheme() {
+    this.setState({ isDarkTheme: !this.state.isDarkTheme });
+  }
+
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
